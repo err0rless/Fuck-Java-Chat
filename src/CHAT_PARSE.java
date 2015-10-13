962bde5ab77d0dbc8f4c5b0e3a1a184c1697a571
err0rless <err0rless313@gmail.com>
2015-10-13 22:56:02 +0900

    add CHAT_PARSE 2015-10-13

---
+++ src/CHAT_PARSE.java
@@ -0,0 +1,74 @@
+/**
+ * Created by err0rless on 2015-10-13.
+ * github.com/err0rless/Fuck-Java-Chat
+ *
+ * Class For Parsing
+ *   .emoticons
+ *   .images or files
+ *   .string
+ *
+ * 1.0: process emoticon
+ */
+
+import java.util.regex.*;
+
+public class CHAT_PARSE
+{
+    private String keywords[] = {"(lol)", "(good)", "(bad)",
+                                 "(what)", "(notgood)", "(prettygood)"};
+    private String emoticon[] = {"XD", ":)", ":(",
+                                 ":/", ":<", ":>"};
+
+    CHAT_PARSE(){} // constructor
+
+    public String Parse_string(String str)
+    {
+        final String EmoticonRegex = "\\([A-z]+\\)"; // "(emoticon)"
+
+        Pattern p = Pattern.compile(EmoticonRegex);
+        Matcher m = p.matcher(str);
+
+        if (m.find(0))
+        {
+            return Parse_Emoticon(str);
+        }
+        else
+        {
+            return str;
+        }
+    }
+
+    public String Parse_Emoticon(String str)
+    {
+        Pattern p = Pattern.compile("\\([A-z]+\\)"); // "(emoticon)"
+        Matcher m = p.matcher(str);
+
+        String s = "", t = "", e[];
+        boolean emot = false;
+
+        while (m.find()) s += m.group(0) + " ";
+
+        str = m.replaceAll("(EMOTICON)");
+
+        e = s.split(" ");
+        for (int i = 0; i < e.length; i++)
+        {
+            for (int j = 0; j < keywords.length; j++)
+            {
+                if (e[i].equals(keywords[j]))
+                {
+                    t = emoticon[j];
+                    emot = true;
+                    break;
+                }
+            }
+
+            if (!emot) t = e[i];
+            emot = false;
+
+            str = str.replaceFirst("\\(EMOTICON\\)", t);
+        }
+
+        return str;
+    }
+}
---
+++ src/Test_Main.java
@@ -0,0 +1,17 @@
+/**
+ * Created by err0rless on 2015-10-13.
+ *
+ * Main class for testing
+ */
+
+public class Test_Main
+{
+    public static void main(String args[])
+    {
+        CHAT_PARSE chatParse = new CHAT_PARSE();
+        String chat = "This is not good (notgood), what is it?! (what)";
+
+        chat = chatParse.Parse_string(chat);
+        System.out.println(chat);
+    }
+}
